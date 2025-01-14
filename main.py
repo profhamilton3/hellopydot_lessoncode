7bb6ce9a246c399d9b2fc4d2f88d99a1bcd6391e
T. Hamilton <profhamilton3@gmail.com>
2025-01-14 14:29:29 -0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+
+
+basic.show_icon(IconNames.HEART)
+basic.show_icon(IconNames.SMALL_HEART)
+
+
+def onEvery_interval():
+    pass
+loops.every_interval(500, onEvery_interval)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.SmallHeart)
+loops.everyInterval(500, function onEvery_interval() {
+    
+})
