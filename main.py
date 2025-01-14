1619c11240c6c707d63a399e4266da2f170a79f0
T. Hamilton <profhamilton3@gmail.com>
2025-01-14 14:54:59 -0800

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 
 
 
+def on_forever():
 
-basic.show_icon(IconNames.HEART)
-basic.show_icon(IconNames.SMALL_HEART)
+    music.play(music.tone_playable(Note.F, music.beat(BeatFraction.SIXTEENTH)), music.PlaybackMode.IN_BACKGROUND)
+    basic.show_icon(IconNames.HEART,300)
+    music.play(music.tone_playable(Note.G, music.beat(BeatFraction.SIXTEENTH)), music.PlaybackMode.IN_BACKGROUND)
+    basic.show_icon(IconNames.SMALL_HEART,150)
+
+basic.forever(on_forever)
+
 
 
 def onEvery_interval():
-    pass
-loops.every_interval(500, onEvery_interval)
+    basic.show_icon(IconNames.STICK_FIGURE,50)
+
+loops.every_interval(1500, onEvery_interval)
--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
-basic.showIcon(IconNames.Heart)
-basic.showIcon(IconNames.SmallHeart)
-loops.everyInterval(500, function onEvery_interval() {
-    
+basic.forever(function on_forever() {
+    music.play(music.tonePlayable(Note.F, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.Heart, 300)
+    music.play(music.tonePlayable(Note.G, music.beat(BeatFraction.Sixteenth)), music.PlaybackMode.InBackground)
+    basic.showIcon(IconNames.SmallHeart, 150)
+})
+loops.everyInterval(1500, function onEvery_interval() {
+    basic.showIcon(IconNames.StickFigure, 50)
 })
